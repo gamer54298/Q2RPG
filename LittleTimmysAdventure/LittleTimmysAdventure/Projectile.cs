16d43714fd54e8ebaa8f562c44b64d5e75f3a3d4
Devyn <kinisongaming@gmail.com>
2018-11-26 21:43:10 -0800

    Add files via upload
    Added 2 new classes

---
+++ LittleTimmysAdventure/LittleTimmysAdventure/Projectile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LittleTimmysAdventure
+{
+    class Projectile:Weapon
+    {
+        public int Capacity;
+        public int Ammunition;
+
+        public Projectile(int capacity, int ammunition)
+        {
+            Capacity = capacity;
+            Ammunition = ammunition;
+        }
+    }
+}
---
+++ LittleTimmysAdventure/LittleTimmysAdventure/Weapon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LittleTimmysAdventure
+{
+    public class Weapon
+    {
+        public int Damage;
+        public int Worth;
+        public bool IsMagical;
+        public bool Modifier;
+
+        public Weapon(int damage, int worth, bool isMagical, bool modifier)
+        {
+            Damage = damage;
+            IsMagical = isMagical;
+            Modifier = modifier;
+            Worth = worth;
+        }
+
+        public Weapon()
+        {
+
+        }
+    } 
+
+}
